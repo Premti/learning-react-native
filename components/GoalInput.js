62481f85cc0d3bc74d036ee6e14f35172aa30ead
Premti <premti@gmail.com>
2020-10-11 19:35:26 +0100

    can enter using input section, styling added, touchableopacity added for it

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -4,5 +4,22 @@ import { View, Text, StyleSheet, TouchableOpacity} from "react-native"
 const GoalItem = (props) => {
 
 
-   
+    return(
+        <TouchableOpacity onPress={props.onDelete.bind(this, props.id)}>
+        <View style={styles.listItem}>
+            <Text>{props.title}</Text>
+            </View>
+            </TouchableOpacity>
+    )
 }
+export default GoalItem
+
+const styles = StyleSheet.create({
+    listItems: {
+        padding: 10,
+        backgroundColor: "grey",
+        borderColor: "black",
+        borderWidth: 1,
+        margin: 10
+      }
+})
